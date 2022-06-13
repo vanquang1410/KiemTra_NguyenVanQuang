de694789a1b54cf4ee360a04c0089246ec20d63b
Văn Quang Nguyễn <nguyenvanquang20001@gmail.com>
2022-06-13 19:44:45 +0700

    update đăng nhập

---
+++ KiemTra_NguyenVanQuang/Controllers/DangNhapController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_NguyenVanQuang.Models;
+namespace KiemTra_NguyenVanQuang.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        // GET: DangNhap
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult ListDangNhap()
+        {        
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
